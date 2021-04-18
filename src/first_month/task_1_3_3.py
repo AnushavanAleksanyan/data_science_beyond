09f0eea5c1f075e6828af53c0359354f5e89d1f1
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-04-19 00:51:38 +0400

    3rd

--- src/first_month/task_1_3_3.py
+++ src/first_month/task_1_3_3.py
@@ -2,10 +2,9 @@
 # Write a Python function which returns factorial value of given number n.
 
 def fact(num):
-	total = 1
-	for number in range(1, num+1):
-		total *= number
-	return total
+	if num ==1:
+		return 1
+	return num * fact(num-1)
 
 
 inp_num = int(input("input a number: "))
@@ -15,6 +14,12 @@ print(fact(inp_num))
 
 # task 2
 # Write a Python function which returns the n-th element of the fibonacci series.
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233 .....
+
 
 def fib(n):
-	
+	if n < 2:
+		return n
+	return fib(n-2)+fib(n-1)
+
+print(fib(7))
