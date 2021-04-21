b4210731c0bf9d386c1af6fee26448ee1fba4403
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-04-21 20:31:38 +0400

    till 4

--- src/first_month/task_1_4_2.py
+++ src/first_month/task_1_4_2.py
@@ -12,26 +12,43 @@ print(add_val("Key","Value", d))
 # task 2
 # Write a python program which concat 2 dicts.
 
-# def concat_dicts(dict1, dict2):
-# 	for item in dict2.items():
-# 		dict1.update(item)
-# 	return dict1
+def concat_dicts(dict1, dict2):
+	dict1.update(dict2)
+	return dict1
 
 d1 = {1:"one", 2:"two"}
 d2 = {3:"there", 4:"four", 5:"five"}
-for item in d2.items():
-	d1.update(item)
-print(d1)
-# print(concat_dicts(d1, d2))
+
+print(concat_dicts(d1,d2))
+
 
 
 # task 3
 # Write a python program, which create a dictionary for given number N, where keys are numbers from 1 to N, and values are cubs of that numbers
 
+def cubes(number):
+	dict = {}
+	for key in range(1, number+1):
+		dict[key] = key**3
+	return dict
+
+print(cubes(5))
+
+
 
 # task 4
 # Write a python program which create dict from 2 lists with the same length
 
+def combine(lst1, lst2):
+	dict = {}
+	for i in range(len(lst1)):
+		dict[lst1[i]]=lst2[i]
+	return dict
+
+l1 = [1, 2, 3, 4, 5, 6]
+l2 = ["one", "two", " there", "four", "five", "six"]
+
+print(combine(l1, l2))
 
 # task 5
 # Write a python program which gets the maximum and minimum values of a dictionary.
