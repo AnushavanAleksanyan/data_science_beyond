62b19652d7cf5942d1671251a7a89eba08fcc359
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-04-21 15:29:36 +0400

    end

--- src/first_month/task_1_4_1.py
+++ src/first_month/task_1_4_1.py
@@ -50,3 +50,45 @@ def unic(lst1, lst2):
 l1 = [1, 5, 7, 8, 3, 12, 2, 9, 7]
 l2 = [8, 15, 17, 8, 5, 22, 7]
 print(unic(l1,l2))
+
+
+# task 5
+# Write a python program which return elements which are or in first, either in second, or in both
+
+def union(lst1, lst2):
+	lst1 = set(lst1)
+	lst2 = set(lst2)
+	result = lst1 | lst2
+	return result
+
+l1 = [1, 5, 7, 8, 3, 12, 2, 9, 7]
+l2 = [8, 15, 17, 8, 5, 22, 7]
+print(union(l1,l2))
+
+
+
+# task 6
+# Write  python function which get set and element value, and remove from set element with given value if exist
+
+def rem(val, set1):
+	set1.discard(val)
+	return set1
+
+s = {4, 28, 9, 16}
+v = 9
+print(rem(v, s))
+
+
+
+# task 6
+# Write a python python program, which return list from given set, where each element of list, is equal to cub of set element
+
+def cube(set2):
+	lst = []
+	for item in set2:
+		lst.append(item**4)
+	return lst
+
+s = {4, 8, 9, 6}
+
+print(cube(s))
