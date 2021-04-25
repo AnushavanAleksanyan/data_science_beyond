f7457fe8ccbe1c84ebadc01bd79d13eafeb14006
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-04-25 11:49:33 +0400

    1.5.2-2

--- src/first_month/task_1_5_2.py
+++ src/first_month/task_1_5_2.py
@@ -5,21 +5,20 @@ class Money():
 		self.amount = amount
 		self.currency = currency
 	def print_obj(self):
-		return self.amount, self.currency
+		print(self.amount, self.currency)
 	def sum(self, other):
 		return self.amount + other.amount
 	def sub(self, other):
 		return self.amount - other.amount
-	def __repr__(self):
-		return f"{self.amount} {self.currency}"
+
 
 
 
 def main():
 	x = Money(11, "USD")
 	y = Money(5, "USD")
-	print(x)
-	print(y)
+	x.print_obj()
+	y.print_obj()
 	print(x.sum(y))
 	print(x.sub(y))
 
