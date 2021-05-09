17cc72ac4555178f64023d72d2ac5714e7533ca2
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-05-09 22:40:08 +0400

    corrected

--- src/first_month/task_1_5_2.py
+++ src/first_month/task_1_5_2.py
@@ -1,15 +1,10 @@
-class MinusError(Exception):
+class MoneyClassError(Exception):
 	def __init__(self, err_text, val):
 		self.err_text = err_text
 		self.val = val
 	def print_obj(self):
 		print(self.err_text, "|", self.val)
 
-class MyTypeError(Exception):
-	def __init__(self, error):
-		self.error = error
-	def __repr__(self):
-		return(self.error)
 
 class Money():
 	rates = {"USD": 523, "EUR": 631, "RUB": 7.05, "AMD": 1}
@@ -17,52 +12,74 @@ class Money():
 	def __init__(self, amount:int, currency:str):
 		try:
 			if int(amount)<0:
-				raise MinusError("Amount is less than 0", amount)
-		except MinusError as me:
+				raise MoneyClassError("Amount is less than 0", amount)
+		except MoneyClassError as me:
 			me.print_obj()
 		else:
 			try:
-				if not (isinstance(amount, int) and isinstance(currency, str)):
-					raise MyTypeError("Type Error")
-			except MyTypeError as mte:
-				print(mte)
+				if not isinstance(currency, str):
+					raise MoneyClassError("currency must be string", currency)
+				elif not isinstance(amount, int):
+					raise MoneyClassError("amount must be number", amount)
+			except MoneyClassError as te:
+				te.print_obj()
 			else:
-				self.amount = amount
-				self.currency = currency
+				self.__amount = amount
+				self.__currency = currency
 
 	def __repr__(self):
-		return f"{self.amount} {self.currency}"
+		return f"{self.__amount} {self.__currency}"
 
-	def sum(self, other):
-		obj = Money(self.amount+other.amount, self.currency)
+	def get_amount(self):
+		print("get")
+		return self.__amount
+
+	def set_amount(self, x):
+		try:
+			if x<0:
+				raise MoneyClassError("Amount is less than 0", x)
+		except MoneyClassError as me:
+			me.print_obj()
+		else:
+			print("set")
+			self.__amount = x
+
+	def del_amount(self):
+		print("del")
+		del self.__amount
+
+	amount = property(get_amount, set_amount, del_amount, doc = "property for amount")
+
+	def __add__(self, other):
+		obj = Money(self.__amount+other.__amount, self.__currency)
 		return obj
 
 	def change(self, rate):
-		obj = Money(
-			self.amount*self.rates[self.currency]/self.rates[rate], rate)
+		obj = Money(self.__amount*self.rates[self.__currency]/self.rates[rate], rate)
 		return obj
 
-	def sub(self, other):
-		result = self.amount-other.amount
+	def __sub__(self, other):
+		result = self.__amount-other.__amount
 		try:
 			if result<=0:
-				raise MinusError("result can't be negative", result)
-		except MinusError as me:
+				raise MoneyClassError("result can't be negative", result)
+		except MoneyClassError as me:
 			me.print_obj()
 		else:
-			obj = Money(self.amount-other.amount, self.currency)
+			obj = Money(self.__amount-other.__amount, self.__currency)
 			return obj
 
 
 
 def main():
-	x = Money(9, 10)
+	x = Money(9, "usd")
 	y = Money(5, "USD")
 	z = Money(10, 'EUR')
-	print(x)
-    # print(y.print_obj())
-	# print(x.sum(y))
-	print(x.sub(y))
+	print(x.amount)
+	x.amount = -10
+	print(x.amount)
+	# print(x+y)
+	# print(x.sub(y))
 	# print(z.change("USD").print_obj())
 
 
