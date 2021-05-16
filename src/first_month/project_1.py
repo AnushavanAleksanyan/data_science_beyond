d41819412c7eaaca7b14b4f5ae43e4c46e3617c2
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-05-16 18:51:56 +0400

    3rd

--- src/first_month/project_1.py
+++ src/first_month/project_1.py
@@ -1,92 +1,95 @@
 class Room:
-	reserved = False
-	room_number = 0
-	count = {"single":0, "double": 0, "penthouse":0}
-
-	def __init__(self, type:str):
+	def __init__(self, type:str, count=1):
 		self.__type = type
-		Room.count[self.__type] += 1
+		self.__count = count
 
 	def __repr__(self):
-		return f"{self.__type}, {self.reserved}"
+		return f"{self.__type}, {self.__count}"
 
 	def get_type(self):
 		return self.__type
 
 	def get_count(self):
-		return f"Free {self.count[self.__type]} '{self.__type}' rooms"
+		return f"Free {self.__count} rooms"
 
-	def reserve(self):
-		if not self.reserved:
-			self.reserved = True
-			Room.count[self.__type] -= 1
+	def reserve(self, type, count):
+		if count<=self.__count:
+			self.__count -= count
+		else:
+			print(f"You can only reserve {self.__count} rooms")
+		return Room(self.__type, self.__count)
 
-	def checkout(self):
-		if self.reserved:
-			self.reserved = False
-			Room.count[self.__type] += 1
+	def checkout(self, type, count):
+		self.__count += count
 
 
 class Hotel(Room):
-	rating = 0
-	rater_count = 0
-	rooms = []
-
 	def __init__(self, name):
-		super().__init__(type)
-		self.name = name
+		self.__name = name
+		self.__rating = 0
+		self.__rater_count = 0
+		self.__rooms = []
+
+
+	def __repr__(self):
+		return f"{self.__name} \nrating - {self.__rating} \nrated {self.__rater_count} people, "
 
 	def rate(self, mark):
-		self.rater_count +=1
-		self.rating = (self.rating + mark)//self.rater_count
+		self.__rater_count +=1
+		self.__rating = (self.__rating + mark)//self.__rater_count
 
 	def get_rating(self):
-		return self.rating
+		return self.__rating
 
 	def add_room(self, room):
-		self.rooms.append(room)
+		self.__rooms.append(room)
 
 	def remove_room(self, room):
-		self.rooms.remove(room)
+		self.__rooms.remove(room)
 
 	def get_rooms(self):
-		return self.rooms
+		return self.__rooms
 
-	def reserve(self):
-		pass
+	# def reserve(self):
+	# 	super().reserve()
 
-	def checkout(self):
-		pass
+	# def checkout(self):
+	# 	pass
 
 
-r1 = Room("single")
-print(r1)
-r1.reserve()
-print(r1.get_count())
-print(r1)
-#print(r1.get_type())
-r1.checkout()
-#print(r1)
-print(r1.get_count())
+r1 = Room("single", 5)
+# print(r1)
+# print(r1.get_type())
+# r1.reserve("single", 2)
+# print(r1.get_count())
+# r1.checkout("single", 1)
+# print(r1)
+#print(r1.get_count())
 
-r2 = Room("double")
+r2 = Room("double", 8)
 # r2.reserve()
 # print(r2)
-print(r2.get_count())
+#print(r2.get_count())
 
 
-# r3 = Room("double")
+r3 = Room("penthouse", 4)
 # r3.reserve()
 # print(r3.get_count())
 print("*************")
 
 
-# h1 = Hotel("MyHotel")
-# h1.add_room(r1)
-# h1.add_room(r2)
-# print(h1.get_rooms())
-
+h1 = Hotel("MyHotel")
+h1.add_room(r1)
+h1.add_room(r2)
+h1.add_room(r3)
+print(h1.get_rooms())
 # h1.rate(10)
 # h1.rate(3)
-# h1.rate(10)
 # print(h1.get_rating())
+
+print(r1.get_count())
+h1.reserve("single", 2)
+print(r1.get_count())
+#print(h1.get_count())
+
+#print(h1)
