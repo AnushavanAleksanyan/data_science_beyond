d60c3bf37c2b5c60ec54568a2af36ba18841a93f
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-05-26 23:03:08 +0400

    Beautiful is better than ugly.

--- src/second_month/task_2_1.py
+++ src/second_month/task_2_1.py
@@ -2,14 +2,12 @@ import numpy as np
 
 # task 2_1_1
 # convert a list of numeric values into a one-dimensional NumPy array
-
 def list_to_array(lst):
 	return np.array(lst)
 
 
 # task 2_1_2
 # create a NumPy array with values ranging from 2 to 10
-
 def create_array():
 	arr = np.arange(2,11)
 	return arr
@@ -17,32 +15,27 @@ def create_array():
 
 # task 2_1_3
 # create a null vector of size 10 and update sixth to eight values to 11
-
 def zeroes():
 	arr = np.zeros(10)
-	for i in range(5,8):
-		arr[i] = 11
+	arr[5:8] = 11
 	return arr
+
 
 # task 2_1_4
 # test whether each element of a 1-D array is also present in a second array.
-
 def compare(arr1, arr2):
-	for elem in arr1:
-		if not elem in arr2:
-			return "False"
-	return "True"
+	return np.in1d(arr1, arr2)
 
 
-l1 = [5,6,8]
-print(list_to_array(l1))   # task 2_1_1
+def main():
+	l1 = [5,6,8]
+	print(list_to_array(l1))   # task 2_1_1
+	print(create_array())    # task 2_1_2
+	print(zeroes())   # test 2_1_3
 
-print(create_array())    # task 2_1_2
+	l2 = np.array([5,6,8,7])
+	l3 = np.array([5,6,8,7,9,3])
+	print(compare(l2, l3))
 
-print(zeroes())   # test 2_1_3
 
-
-l2 = np.array([5,6,8,7])
-l3 = np.array([5,6,8,7,9,3])
-
-print(compare(l2, l3))
+main()
--- src/second_month/task_2_2.py
+++ src/second_month/task_2_2.py
@@ -3,64 +3,56 @@ from numpy.linalg import inv, det
 
 # task_2_2_1
 # Write a NumPy program to compute the multiplication of two given matrixes
-
 def mul(a1,a2):
 	return a1.dot(a2)
 
 
-arr1 = np.arange(1,12,3)
-arr2 = np.arange(10,2,-2)
-arr3 = np.array([[1, 2, 3],[7,8,9]])
-arr4 = np.array([[2,3, 1],[3,5, 5],[1,4, 8]])
-print(arr4[0])
-arr2[1:2]=55
-print(arr3.T)
-print(arr3.dot(arr4))
-print(mul(arr3,arr4))
-
-
 # task_2_2_2
 # Write a NumPy program to compute the determinant of an array
-
 def determinant(arr):
 	return det(arr)
-
-print("determinant")
-print(determinant(arr4))
-
 
 
 # task_2_2_3
 # Write a NumPy program to compute the sum of the diagonal element of a given array
-
 def diag_sum(arr):
-	item = 0
-	res = 0
-	for elem in arr:
-		res += elem[item]
-		item += 1
-	return res
-
-
-print(diag_sum(arr4))
+	return np.trace(arr)
 
 
 # task_2_2_4
 # Write a NumPy program to compute the inverse of a given matrix
-
 def inverse(arr):
 	return inv(arr)
-
-print (inverse(arr4))
 
 
 # task_2_2_5
 # Write a NumPy program to generate matrix and write it to a file, then again read from file that matrix.
-
-def mat_to_file():
-	arr = np.random.randn(5,5)
+def mat_to_file(low, hight):
+	arr = np.random.randint(low, hight, size=(3, 6))
 	np.save('file', arr)
 	info = np.load('file.npy')
 	print(info)
 
-mat_to_file()
+
+def main():
+	arr1 = np.arange(1,12,3)
+	arr2 = np.arange(10,2,-2)
+	arr3 = np.array([[1, 2, 3],[7,8,9]])
+	arr4 = np.array([[2,3, 1],[3,5, 5],[1,4, 8]])
+	print(arr4[0])
+	arr2[1:2]=55
+	print(arr3.T)
+	print(arr3.dot(arr4))
+	print(mul(arr3,arr4))   # task.2.2.1
+
+	print("determinant")    # task.2.2.2
+	print(determinant(arr4))
+
+	print(diag_sum(arr4))   # task.2.2.3
+
+	print (inverse(arr4))   # task.2.2.4
+
+	mat_to_file(2,6)        # task.2.2.5
+
+
+main()
