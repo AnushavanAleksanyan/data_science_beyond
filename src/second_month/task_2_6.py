a583f81cf51f06b9430ea9b4589489a4d73ca6ff
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-06-10 22:45:45 +0400

    2-6-4

--- src/second_month/task_2_6.py
+++ src/second_month/task_2_6.py
@@ -15,9 +15,9 @@ def group_by_school(dataf):
 # task_2_6_2
 # Write a Pandas program to split the following given dataframe by school code and get mean, min, and max value of age for each school
 def mean_min_max(dataf):
-	mean = dataf.groupby("school")['age'].mean()
-	min = dataf.groupby("school")['age'].min()
-	max = dataf.groupby("school")['age'].max()
+	mean = dataf.groupby("school")['age'].mean().reset_index(name="mean")
+	min = dataf.groupby("school")['age'].min().reset_index(name="min")
+	max = dataf.groupby("school")['age'].max().reset_index(name="max")
 	print(pd.concat([mean, min, max], axis=1))
 
 
@@ -29,28 +29,35 @@ def group_by_school_class(dataf):
 		print(grouped.get_group(key))
 
 # task_2_6_4
-# Write a Pandas program to split the following given dataframe into groups based on school code and cast grouping as a list
+# Write a Pandas program to split the following given dataframe into groups based on school code
+# and cast grouping as a list
 def group_by_school_list(dataf):
-	gb = dataf.groupby('school')
-	print (gb.groups)
-	# for key, value in gb:
-	# 	print(gb.get_group(key))
+	print(type(dataf))
+	print(dataf.groupby("school").agg(list))
 
 
 # task_2_6_5
-# Write a Pandas program to split a dataset, group by one column and get mean, min, and max values by group. Using the following dataset find the mean, min, and max values of purchase amount (purch_amt) group by customer id (customer_id)
+# Write a Pandas program to split a dataset, group by one column and get mean, min, and max values by group.
+# Using the following dataset find the mean, min, and max values of purchase amount (purch_amt) group by customer id (customer_id)
+df2 = pd.read_csv("other/task-2-6/restourant.csv")
+def mean_min_max_rest(dataf, col, by_col):
+	mean = dataf.groupby(col)[by_col].mean()
+	min = dataf.groupby(col)[by_col].min()
+	max = dataf.groupby(col)[by_col].max()
+	print(pd.concat([mean, min, max], axis=1))
 
 
 def main():
-	# print("<task2_6_1>")
-	# group_by_school(df)  # task2_6_1
-	# print("<task2_6_2>")
-	# mean_min_max(df)     # task2_6_2
-	# print("<task2_6_3>")
-	# group_by_school_class(df)   # task2_6_3
-	# print("<task2_6_4>")
-	group_by_school_list(df)
-
+	print("<task2_6_1>")
+	group_by_school(df)  # task2_6_1
+	print("\n<task2_6_2>")
+	mean_min_max(df)     # task2_6_2
+	print("\n<task2_6_3>")
+	group_by_school_class(df)   # task2_6_3
+	print("\n<task2_6_4>")
+	group_by_school_list(df)     # task2_6_4
+	print("\n<task2_6_5>")
+	mean_min_max_rest(df2, "customer_id", "purch_amt")     # task2_6_5
 
 
 
