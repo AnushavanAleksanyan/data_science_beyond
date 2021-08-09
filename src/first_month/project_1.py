95313330be0f6496e67449eeb0c1f7270c9e57fd
AnushavanAleksanyan <a-a-2-0-0-7@yandex.ru>
2021-08-09 10:02:10 +0400

    ds2

--- src/first_month/project_1.py
+++ src/first_month/project_1.py
@@ -1,10 +1,12 @@
+# Create a reservation system which books  hotel rooms.
+# It charges various rates for particular sections of the hotel
 class Room:
-	def __init__(self, type:str, count):
+	def __init__(self, type, count):
 		self.__type = type
-		self.__count = count
+		self.__count = count    # free rooms
 
 	def __repr__(self):
-		return f"{self.__type}, {self.__count}"
+		return f"{self.__count} {self.__type} rooms"
 
 	def get_type(self):
 		return self.__type
@@ -12,33 +14,48 @@ class Room:
 	def get_count(self):
 		return self.__count
 
-	def reserve(self, type, count):
-		if count<=self.__count:
+	def reserve(self, count):
+		if count <= self.__count:
 			self.__count -= count
 		else:
-			print(f"You can only reserve {self.__count} rooms")
-		#return Room(self.__type, self.__count)    # to be deleted
+			print(f"We have only {self.__count} free {self.__type} rooms")
 
-	def checkout(self, type, count):
+	def checkout(self):
 		self.__count += count
 
 
-class Hotel():
-	def __init__(self, name):
-		self.__name = name
+
+class Hotel:
+	def __init__(self):
+		self.__name = ""
 		self.__rating = 0
 		self.__rater_count = 0
 		self.__rooms = []
 
 	def __repr__(self):
-		return f"{self.__name} \nrating - {self.__rating} \nrated {self.__rater_count} people, "
-
-	def rate(self, mark):
-		self.__rater_count +=1
-		self.__rating = (self.__rating + mark)//self.__rater_count
+		return self.__name
 
 	def get_rating(self):
 		return self.__rating
+
+	def get_rooms(self):
+		return self.__rooms
+
+	def reserve(self, room, count):
+		if room in self.__rooms:
+			room.reserve(count)
+		else:
+			print("We don't have such room")
+
+	def checkout(self):
+		if room in self.__rooms:
+			room.checkout(count)
+		else:
+			print("We don't have such room")
+
+	def rate(self, mark):
+		self.__rater_count += 1
+		self.__rating = (self.__rating + mark)/self.__rater_count
 
 	def add_room(self, room):
 		self.__rooms.append(room)
@@ -46,48 +63,20 @@ class Hotel():
 	def remove_room(self, room):
 		self.__rooms.remove(room)
 
-	def get_rooms(self):
-		return self.__rooms
-
-	def reserve(self):
-		return Room().reserve()
-
-	def checkout(self):
-		pass
-
-
-r1 = Room("single", 5)
+# code samples
+r1 = Room("Single", 10)
+r1.reserve(13)
 print(r1)
-print(r1.get_type())
-# r1.reserve("single", 2)
-# print(r1.get_count())
-# r1.checkout("single", 1)
-# print(r1)
-#print(r1.get_count())
-
-r2 = Room("double", 8)
-print(r2)
-print(r2.get_count())
+r2 = Room("Double", 15)
 
 
-# r3 = Room("penthouse", 4)
-# r3.reserve()
-# print(r3.get_count())
-print("*************")
-
-
-h1 = Hotel("MyHotel")
+h1 = Hotel()
+h1.name = "Malibu"
+h1.rate(10)
+h1.rate(8)
+print(h1)
 h1.add_room(r1)
 h1.add_room(r2)
-# h1.add_room(r3)
 print(h1.get_rooms())
-# h1.rate(10)
-# h1.rate(3)
-# print(h1.get_rating())
-
-# print(r1.get_count())
-h1.reserve("single", 2)
-# print(r1.get_count())
-#print(h1.get_count())
-
-#print(h1)
+h1.reserve(r1, 2)
+print(h1.get_rating())
